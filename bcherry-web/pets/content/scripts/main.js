01fc2662da3618ce31402fdad2a8629e878e811e
Ben Cherry <bcherry@gmail.com>
2009-10-30 19:42:39 -0700

    javascript foundations

--- bcherry-web/pets/content/scripts/main.js
+++ bcherry-web/pets/content/scripts/main.js
@@ -1,5 +1,19 @@
+;(function(){
+	var canvas = {dom: null, ctx: null, h: null, w: null};
+	var gameRes = {w: 2, h: 2};
+	var mapPixel = function(x, y) {
+		return {
+			x: (canvas.w / gameRes.w) * x,
+			y: (canvas.h / gameRes.h) * y
+		};
+	};
 $(function(){
-	var theCanvas = $("canvas").get(0).getContext('2d');
-	theCanvas.fillRect(10, 10, 40, 40);
+	var canvas = {dom:$("canvas").get(0)};
+	canvas.ctx = canvas.dom.getContext('2d');
+	canvas.w = canvas.dom.width;
+	canvas.h = canvas.dom.height;
+
+	console.log(canvas.dom.height);
 });
+})();
 
