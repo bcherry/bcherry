e14708737c3eec316ffdb171cebe93acbe658449
Ben Cherry <bcherry@gmail.com>
2009-11-01 01:23:00 -0700

    some sweet canvas work

--- bcherry-web/pets/content/scripts/main.js
+++ bcherry-web/pets/content/scripts/main.js
@@ -1,12 +1,37 @@
 ;(function(){
+	var X_RES = 100;
+	var Y_RES = 60;
+	var pc = null;
+
+	// demo functions
+	var checkerboard = function() {
+		for (var y = 0; y < Y_RES; y++) {
+			for (var x = y % 2; x < X_RES; x += 2) {
+				pc.drawPixel(x, y, "#8cc4ff");
+			}
+			for (var x = (y + 1) % 2; x < X_RES; x += 2) {
+				pc.drawPixel(x, y, "#003b7a");
+			}
+		}
+	};
+
+	var spriteTest = function() {
+		var sprite = [
+			["#8cc4ff", "#003b7a", "#003b7a", "#8cc4ff"],
+			["#003b7a", "#8cc4ff", "#8cc4ff", "#003b7a"],
+			["#003b7a", "#8cc4ff", "#8cc4ff", "#003b7a"],
+			["#8cc4ff", "#003b7a", "#003b7a", "#8cc4ff"]
+		];
+
+		pc.drawSprite(sprite, 2, 2);
+	};
+
 $(function(){
 	var canvas = $("canvas").get(0);
-	var pc = new PixelCanvas.Canvas(canvas, 10, 6);
-	for (var y = 0; y < 6; y++) {
-		for (var x = y % 2; x < 10; x+=2) {
-			pc.fillRect(x, y, 1, 1);
-		}
-	}
+	pc = new PixelCanvas.Canvas(canvas, X_RES, Y_RES);
+	
+	//spriteTest();
+	checkerboard();
 });
 })();
 
--- bcherry-web/pets/content/scripts/pixelCanvas.js
+++ bcherry-web/pets/content/scripts/pixelCanvas.js
@@ -6,11 +6,26 @@
 		var pixelHeight = domCanvas.height / yRes;
 
 		this.fillRect = function(x, y, w, h) {
-			domCanvas.getContext('2d').fillRect(
+			var context = domCanvas.getContext('2d');
+			context.fillRect(
 				pixelWidth * x,
 				pixelHeight * y,
 				pixelWidth * w,
 				pixelHeight * h);
+		};
+
+		this.drawPixel = function(x, y, style) {
+			var context = domCanvas.getContext('2d');
+			context.fillStyle = style;
+			context.fillRect(pixelWidth * x, pixelHeight * y, pixelWidth, pixelHeight);
+		};
+
+		this.drawSprite = function(sprite, x, y) {
+			for (var i = 0; i < sprite.length; i++) {
+				for (var j = 0; j < sprite[i].length; j++) {
+					this.drawPixel(x + j, y + i, sprite[i][j]);
+				}
+			}
 		};
 	};
 })();
