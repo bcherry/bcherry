a7ae9e85562644ba478d22dd11bd99929aabbe0e
Ben Cherry <bcherry@gmail.com>
2009-10-31 23:17:33 -0700

    added some missing files

--- bcherry-web/pets/content/scripts/main.js
+++ bcherry-web/pets/content/scripts/main.js
@@ -7,13 +7,19 @@
 			y: (canvas.h / gameRes.h) * y
 		};
 	};
+	var getWidth = function() { return canvas.w / gameRes.w; };
+	var getHeight = function() { return canvas.h / gameRes.h; };
+	var fillPixel = function(x, y) {
+		canvas.ctx.fillRect(mapPixel(x), mapPixel(y), getWidth(), getHeight());
+	};
 $(function(){
 	var canvas = {dom:$("canvas").get(0)};
 	canvas.ctx = canvas.dom.getContext('2d');
 	canvas.w = canvas.dom.width;
 	canvas.h = canvas.dom.height;
 
-	console.log(canvas.dom.height);
+	fillPixel(0,1);
+	
 });
 })();
 
