63e12bffba2065b84d4c49b4e8476f06b57cfdaf
Ben Cherry <bcherry@gmail.com>
2009-11-18 22:52:20 -0800

    first version of consul.js

---
+++ bcherry-web/static/js/consul.js
@@ -0,0 +1,112 @@
+;(function(){
+	var _consul= function() {
+		
+		// consul defaults to on, feel free to change this
+		var on = true;
+		
+		// This function can turn consul on or off (default is on)
+		this.turnOn = function(bool) { on = (bool == undefined || bool); };
+		
+		// These are the browser JS consoles that consul supports
+		// Each tool needs two fields:
+		// 		bit - A unique int, so we can pack browser support bitwise into an integer
+		//		detect - A function that checks for the presence of the debugging tools
+		var tools = {
+			// Firebug for Firefox, on all platforms (not Firebug Lite for other browsers)
+			"firebug"		: { bit : 1, detect : function() { return window.console && "firebug" in window.console; } },
+			
+			// Google Chrome and Chromium for Windows, Mac, and Linux
+			"chromium"		: { bit : 2, detect : function() { return window.console && "chromium" in window; } },
+			
+			// Internet Explorer, provided window.console exists (IE8+ I think, this might also catch Firebug Lite actually)
+			"ie"			: { bit : 3, detect : function() { return /MSIE/.test(navigator.userAgent) && window.console; } },
+			
+			// Opera on all platforms.  Dragonfly does not yet support console.*, but shows messages from opera.postError
+			"opera"			: { bit : 4, detect : function() { return "opera" in window && "postError" in window.opera; } },
+			
+			// Apple Safari, on Mac and Windows
+			"safari"		: { bit : 5, detect : function() { return /Safari/.test(navigator.userAgent) && window.console; } },
+			
+			// Firebug Lite, a cross-browser implementation of the basic Firebug features.  Could break on versions < 1.2.
+			"firebuglite"	: { bit : 6, detect : function() { return "firebug" in window; } }
+		};
+		
+		// Takes an arbitrary number of browser ids (like above), and returns an int representing all of them
+		var b = function() {
+			var args = Array.prototype.slice.call(arguments);
+			var val = 0;
+			for (var i = 0, n = args.length; i < n; i++) {
+				val += (1 << tools[args[i]].bit);
+			}
+			return val;
+		};
+		
+		// These are the functions that will be ported from window.console to our console
+		// Each entry should use the b() function above to mark browsers which support it
+		var consoleFuncs = {
+			log			: b("firebug", "firebuglite", "chromium", "safari", "ie", "opera"),
+			debug		: b("firebug", "firebuglite", "chromium", "safari"),
+			info		: b("firebug", "firebuglite", "chromium", "safari", "ie", "opera"),
+			warn		: b("firebug", "firebuglite", "chromium", "safari", "ie", "opera"),
+			error		: b("firebug", "firebuglite", "chromium", "safari", "ie", "opera"),
+			assert		: b("firebug", "firebuglite", "chromium", "safari", "ie", "opera"),
+			dir			: b("firebug", "firebuglite", "chromium", "safari"),
+			dirxml		: b("firebug", "firebuglite", "chromium", "safari"),
+			trace		: b("firebug", "firebuglite", "chromium", "safari"),
+			group		: b("firebug", "firebuglite", "chromium", "safari"),
+			groupEnd	: b("firebug", "firebuglite", "chromium", "safari"),
+			time		: b("firebug", "firebuglite", "chromium", "safari"),
+			timeEnd		: b("firebug", "firebuglite", "chromium", "safari"),
+			count		: b("firebug", "firebuglite", "chromium", "safari")
+		};
+		
+		// Tries to detect a browser which supports the given function, returning the result
+		var isSupported = function(func) {
+			for (var prop in tools) {
+				if (tools.hasOwnProperty(prop)) {
+					var tool = tools[prop];
+					if (tool.detect() && (consoleFuncs[func] & (1 << tool.bit))) {
+						return true;
+					}
+				}
+			}
+			return false;
+		};
+		
+		// Wraps a given function from window.console with browser and on/off detection
+		var generateFn = function(prop) {
+			return function() {
+				if (on && isSupported(prop)) {
+					// This method is supported by our available tools, so let's work through them in order of priority
+					if (tools.firebug.detect() || tools.chromium.detect() || tools.safari.detect()) {
+						// For browsers that don't suck, just re-apply the arguments to the desired window.console method
+						window.console[prop].apply(window.console, arguments);
+					} else if (tools.firebuglite.detect()) {
+						// Firebug Lite doesn't suck, but it uses firebug.d.console instead of window.console
+						window.firebug.d.console[prop].apply(window.firebug.d.console, arguments);
+					} else if (tools.opera.detect()) {
+						// Opera doesn't suck, per-se, but it also doesn't have any console.* methods AT ALL
+						// So, instead, we pass everything along to opera.postError as for IE, but prepended with the method name
+						window.opera.postError(prop.toUpperCase() + Array.prototype.join.call(arguments, ', '));
+					} else if (tools.ie.detect()) {
+						// For IE dev toolbar, window.console methods aren't real functions, or something.  And they have shitty features.
+						// ...so just string-concatenate all the arguments and pass that along to the right "function"
+						window.console[prop](Array.prototype.join.call(arguments, ', '));
+					}
+				}
+			};
+		};
+		
+		// Loop through our defined functions and assign them
+		for (var prop in consoleFuncs) {
+			// Skip extra Object.prototype properties
+			if (consoleFuncs.hasOwnProperty(prop)) {
+				// Apply our generator (this allows us to avoid closure issues with the loop)
+				this[prop] = generateFn(prop);
+			}
+		}
+	};
+
+	// Assign to window.consul
+	window.consul = new _consul();
+})();
