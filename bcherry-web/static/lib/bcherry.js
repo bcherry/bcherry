bb33634302d8d91641113214c31259bc55650be8
Ben Cherry <bcherry@gmail.com>
2009-12-06 19:58:00 -0800

    bcherry.js

---
+++ bcherry-web/static/lib/bcherry.js
@@ -0,0 +1,10 @@
+var isNumber = function isNumber(value) {
+	return typeof value === "number" && isFinite(value);
+}
+
+var isArray = function isArray(value, strict) {
+	if (strict) {
+		return value && typeof value === "object" && typeof value.length === "number" && !(value.propertyIsEnumerable("length"));
+	}
+	return value && typeof value === "object" && value.constructor === Array;
+}
