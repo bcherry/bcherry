d27704025a64edefbe253123d342f9ee2b09be8f
Ben Cherry <bcherry@gmail.com>
2010-02-11 10:19:01 -0800

    enhanced badglobals.js

--- bcherry-web/static/lib/js/badglobals.js
+++ bcherry-web/static/lib/js/badglobals.js
@@ -3,41 +3,67 @@
 
 // Include this script BEFORE any scripts you want to scan for bad globals.
 // After the scripts you want to scan, run BADGLOBALS.check() and watch firebug.
+// This script will only work in Firefox or Chrome, NOT in Internet Explorer.
 var BADGLOBALS = (function (window) {
-	var report = {
-			badGlobals: []
-		},		
-		skip = {},
+	var report = {},		
+		skip = [],
 		that = {
-			check: function () {
+			check: function (exclude) {
 				var prop,
-					val;
+					val,
+					bad = [],
+					good = [],
+					skipped = [];
 				
-				report.badGlobals = [];
+				exclude = exclude || [];
+				
 				for (prop in window) {
-					if (window.hasOwnProperty(prop) && !skip.hasOwnProperty(prop)) {
-						val = window[prop];
-						if (delete window[prop]) {
-							console.warn("Found non-var global %o with value %o", prop, val);
-							report.badGlobals.push(prop);
-							try {
-								window[prop] = val;
-							} catch (e) {
-								console.error("Oops, there was an error putting %o back :(", prop);
+					if (window.hasOwnProperty(prop)) {
+						if (skip.indexOf(prop) >= 0 || exclude.indexOf(prop) >= 0) {
+							skipped.push(prop);
+						} else {
+							val = window[prop];
+							if (delete window[prop]) {
+								console.warn("Found non-var global %o with value %o", prop, val);
+								bad.push(prop);
+								try {
+									window[prop] = val;
+								} catch (e) {
+									console.error("Oops, there was an error putting %o back :(", prop);
+								}
+							} else {
+								good.push(prop);
 							}
 						}
 					}
+				}
+				report = {
+					bad: bad,
+					good: good,
+					skipped: skipped
+				}
+			},
+			exclude: function (names) {
+				var i = 0,
+					l;
+				
+				if (typeof names === "string") {
+					names = [names];
+				}
+				
+				for (l = names.length; i < l; i += 1) {
+					skip.push(names[i]);
 				}
 			},
 			report: function () {
 				return report;
 			}
-		},		
+		},
 		prop;
 	
 	for (prop in window) {
 		if (window.hasOwnProperty(prop)) {
-			skip[prop] = 1;
+			skip.push(prop);
 		}
 	}
 	
