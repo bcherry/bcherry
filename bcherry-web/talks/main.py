c14638812236b5ecd8f010fa87d6bad60b9c682d
Ben Cherry <bcherry@gmail.com>
2010-03-19 17:35:20 -0700

    basically working new presentation directory

---
+++ bcherry-web/talks/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import wsgiref.handlers
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+class MainHandler(webapp.RequestHandler):
+	def get(self):
+		path = os.path.join(os.path.dirname(__file__), 'index.html')
+		self.response.out.write(template.render(path, {}))
+
+class TalkHandler(webapp.RequestHandler):
+	def get(self):
+		talk_name = 'js-better-faster'
+		path = os.path.join(os.path.dirname(__file__), 'talk.html')
+		self.response.out.write(template.render(path, dict(talk_name=talk_name)))
+
+class SlidesHandler(webapp.RequestHandler):
+	def get(self):
+		talk_name = 'js-better-faster'
+		path = os.path.join(os.path.dirname(__file__), talk_name + '/slides.html')
+		self.response.out.write(template.render(path, {}))
+
+def main():
+	application = webapp.WSGIApplication([
+		('/talks/.+/slides/?', SlidesHandler),
+		('/talks/.+', TalkHandler),
+		('/talks/?', MainHandler),
+	], debug=True)
+	wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == '__main__':
+	main()
+
---
+++ bcherry-web/talks/static/js/showoff.js
@@ -0,0 +1,287 @@
+/* ShowOff JS Logic */
+
+var preso_started = false
+var slidenum = 0
+var slideTotal = 0
+var slides
+var totalslides = 0
+var slidesLoaded = false
+var incrSteps = 0
+var incrElem
+var incrCurr = 0
+var incrCode = false
+var debugMode = false
+
+function setupPreso() {
+  if (preso_started)
+  {
+     alert("already started")
+     return
+  }
+  preso_started = true
+
+  loadSlides()
+  doDebugStuff()
+
+  // bind event handlers
+  document.onkeydown = keyDown
+  /* window.onresize  = resized; */
+  /* window.onscroll = scrolled; */
+  /* window.onunload = unloaded; */
+}
+
+function loadSlides() {
+  $('#slides').hide();
+  $("#slides").load("/talks/{TALK_NAME}/slides".split("{TALK_NAME}").join(TALK_NAME), false, function(){
+    slides = $('#slides > .slide')
+    slideTotal = slides.size()
+    setupMenu()
+    if (slidesLoaded) {
+      showSlide()
+      alert('slides loaded')
+    } else {
+      showFirstSlide()
+      slidesLoaded = true
+    }
+    sh_highlightDocument('talks/static/js/sh_lang/', '.min.js')
+   })
+}
+
+function setupMenu() {
+  $('#navmenu').hide();
+
+  var currSlide = 0
+  var menu = new ListMenu()
+  
+  slides.each(function(s, elem) {
+    shortTxt = $(elem).text().substr(0, 20)
+    path = $(elem).attr('ref').split('/')
+    currSlide += 1
+    menu.addItem(path, shortTxt, currSlide)
+  })
+
+  $('#navigation').html(menu.getList())
+  $('#navmenu').menu({ 
+    content: $('#navigation').html(),
+    flyOut: true
+  });
+}
+
+function gotoSlide(slideNum) {
+  slidenum = parseInt(slideNum)
+  if (!isNaN(slidenum)) {
+    showSlide()
+  }
+}
+
+function showFirstSlide() {
+  slidenum = 0
+  showSlide()
+}
+
+function showSlide(back_step) {
+
+  if(slidenum < 0) {
+    slidenum = 0
+    return
+  }
+
+  if(slidenum > (slideTotal - 1)) {
+    slidenum = slideTotal - 1
+    return
+  }
+
+  // TODO: calculate and set the height margins on slide load, not here
+
+  $("#preso").html(slides.eq(slidenum).clone())
+  curr_slide = $("#preso > .slide")
+  var slide_height = curr_slide.height()
+  var mar_top = (0.5 * parseFloat($("#preso").height())) - (0.5 * parseFloat(slide_height))
+  $("#preso > .slide").css('margin-top', mar_top)
+
+  percent = getSlidePercent()
+  $("#slideInfo").text((slidenum + 1) + '/' + slideTotal + '  - ' + percent + '%')
+
+  if(!back_step) {
+    // determine if there are incremental bullets to show
+    // unless we are moving backward
+    determineIncremental()
+  } else {
+    incrCurr = 0
+    incrSteps = 0
+  }
+
+  removeResults()
+}
+
+function getSlidePercent()
+{
+  return Math.ceil(((slidenum + 1) / slideTotal) * 100)
+}
+
+function determineIncremental()
+{
+  incrCurr = 0
+  incrCode = false
+  incrElem = $("#preso > .incremental > ul > li")
+  incrSteps = incrElem.size()
+  if(incrSteps == 0) {
+    // also look for commandline
+    incrElem = $("#preso > .incremental > pre > code > code")
+    incrSteps = incrElem.size()
+    incrCode = true
+  }
+  incrElem.each(function(s, elem) {
+    $(elem).hide()
+  })
+}
+
+function nextStep()
+{
+  if (incrCurr >= incrSteps) {
+    slidenum++
+    showSlide()
+  } else {
+    elem = incrElem.eq(incrCurr)
+    if (incrCode && elem.hasClass('command')) {
+      incrElem.eq(incrCurr).show().jTypeWriter({duration:1.0})
+    } else {
+      incrElem.eq(incrCurr).show()
+    }
+    incrCurr++
+  }
+}
+
+function doDebugStuff()
+{
+  if (debugMode) {
+    $('#debugInfo').show()
+    debug('debug mode on')
+  } else {
+    $('#debugInfo').hide()    
+  }
+}
+
+function debug(data)
+{
+  $('#debugInfo').text(data)
+}
+//  See e.g. http://www.quirksmode.org/js/events/keys.html for keycodes
+function keyDown(event)
+{
+    var key = event.keyCode;
+
+    if (event.ctrlKey || event.altKey || event.metaKey)
+       return true;
+
+    debug('key: ' + key)
+
+    if (key == 32) // space bar
+    {
+      nextStep()
+    }
+    else if (key == 68) // 'd' for debug
+    {
+      debugMode = !debugMode
+      doDebugStuff()
+    }
+    else if (key == 37 || key == 33) // Left arrow OR page up
+    {
+      slidenum--
+      showSlide(true) // We show the slide fully loaded
+    }
+    else if (key == 39 || key == 34) // Right arrow OR page down
+    {
+      nextStep()
+    }
+    else if (key == 82) // R for reload
+    {
+      if (confirm('really reload slides?')) {
+        loadSlides()
+        showSlide()
+      }
+    }
+    else if (key == 84 || key == 67)  // T or C for table of contents
+    {
+      $('#navmenu').toggle().trigger('click')
+    }
+    else if (key == 90) // z for help
+    {
+      $('#help').toggle()
+    }
+    else if (key == 70) // f for footer
+    {
+      $('#footer').toggle()
+    }
+	else if (key == 27) // esc
+	{
+		removeResults();
+	}
+    return true
+}
+
+
+function ListMenu(s)
+{
+  this.slide = s
+  this.typeName = 'ListMenu'
+  this.itemLength = 0;
+  this.items = new Array();
+  this.addItem = function (key, text, slide) {
+    if (key.length > 1) {
+      thisKey = key.shift()
+      if (!this.items[thisKey]) {
+        this.items[thisKey] = new ListMenu(slide)
+      }
+      this.items[thisKey].addItem(key, text, slide)
+    } else {
+      thisKey = key.shift()
+      this.items[thisKey] = new ListMenuItem(text, slide)
+    }
+  }
+  this.getList = function() {
+    var newMenu = $("<ul>")
+    for(var i in this.items) {
+      var item = this.items[i]
+      var domItem = $("<li>")
+      if (item.typeName == 'ListMenu') {
+        choice = $("<a rel=\"" + (item.slide - 1) + "\" href=\"#\">" + i + "</a>")
+        domItem.append(choice)
+        domItem.append(item.getList())
+      }
+      if (item.typeName == 'ListMenuItem') {
+        choice = $("<a rel=\"" + (item.slide - 1) + "\" href=\"#\">" + item.slide + '. ' + item.textName + "</a>")
+        domItem.append(choice)
+      }
+      newMenu.append(domItem)
+    }
+    return newMenu      
+  }
+}
+
+function ListMenuItem(t, s)
+{
+  this.typeName = "ListMenuItem"
+  this.slide = s
+  this.textName = t
+}
+
+var removeResults = function() {
+  $('.results').remove();	
+};
+
+var print = function(text) {
+  removeResults();
+  var _results = $('<div>').addClass('results').text($.print(text));
+  $('body').append(_results);
+  _results.click(removeResults);
+};
+
+$('.sh_javaScript code').live("click", function() {
+  result = null;
+  var codeDiv = $(this);
+  codeDiv.addClass("executing");
+  eval(codeDiv.text());
+  setTimeout(function() { codeDiv.removeClass("executing");}, 250 );
+  if (result != null) print(result);
+});
